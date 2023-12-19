8d1c5a9e15cca3375fe1167dc67cd2e61143c36c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2023-12-18 21:11:51 -0500

    feat: add support to .js and .css static files

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,24 @@ import fs from 'fs'
 
 const server = http.createServer((req, res) => {
 
+    console.log(req.url)
+
     if (req.url === '/') {
         const htmlData = fs.readFileSync('./public/index.html', 'utf-8')
         res.writeHead(200, { 'Content-Type': 'text/html' })
         res.end(htmlData)
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/html' })
-        res.end()
+        return
+    }
+
+    if (req.url?.endsWith('.js')) {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' })
+    } else if (req.url?.endsWith('.css')) {
+        res.writeHead(200, { 'Content-Type': 'text/css' })
+    }
+
+    if (req.url !== '/favicon.ico') {
+        const data = fs.readFileSync(`./public${req.url}`, 'utf-8')
+        res.end(data)
     }
 })
 
