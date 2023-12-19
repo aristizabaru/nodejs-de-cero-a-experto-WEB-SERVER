82f4e7319c6c5927568c8aff1ab89ee17289e100
Andrés Aristizábal <andres.aristizabal@gmail.com>
2023-12-19 04:49:03 -0500

    feat: add SPA with Router
    Use Express to serve SPA with Router

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import path from 'path'
 
 export class Server {
 
@@ -10,6 +11,13 @@ export class Server {
 
         // Public folder
         this.app.use(express.static('public'))
+
+        this.app.get('*', (req, res) => {
+            console.log(req.url)
+
+            const indexPath = path.join(__dirname + '../../../public/index.html')
+            res.sendFile(indexPath)
+        })
 
         this.app.listen(3000, () => {
             console.log(`Server running on port ${3000}`)
